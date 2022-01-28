8f3719e8990ab62ff7aca7db229c5f046fdfa6ec
Michael Penick <michael.penick@datastax.com>
2022-01-28 16:21:40 -0500

    Return server-side cql version for supported options

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -356,7 +356,7 @@ func (c *client) Receive(reader io.Reader) error {
 
 	switch msg := body.Message.(type) {
 	case *message.Options:
-		c.send(raw.Header, &message.Supported{Options: map[string][]string{"CQL_VERSION": {"3.0.0"}, "COMPRESSION": {}}})
+		c.send(raw.Header, &message.Supported{Options: map[string][]string{"CQL_VERSION": {c.proxy.cluster.Info.CQLVersion}, "COMPRESSION": {}}})
 	case *message.Startup:
 		c.send(raw.Header, &message.Ready{})
 	case *message.Register:
