1cb355aebef9bb2a9521b7cb30d0911a2099c3cc
Michael Penick <michael.penick@datastax.com>
2022-02-02 12:15:54 -0500

    Add header comment

--- proxy/run_test.go
+++ proxy/run_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) DataStax, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package proxy
 
 import (
@@ -32,13 +46,14 @@ func TestRun_HealthChecks(t *testing.T) {
 	defer cluster.Shutdown()
 
 	go func() {
-		require.Equal(t, 0, Run(ctx, []string{
+		rc := Run(ctx, []string{
 			"--contact-points", testClusterContactPoint,
 			"--port", strconv.Itoa(testClusterPort),
 			"--health-check",
 			"--http-bind", testProxyHTTPBind,
 			"--readiness-timeout", "200ms", // Use short timeout for the test
-		}))
+		})
+		require.Equal(t, 0, rc)
 	}()
 
 	waitUntil(10*time.Second, func() bool {
