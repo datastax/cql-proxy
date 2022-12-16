e7f2ef1e4ec5ad3d6ee5792c8acd3fcaa43caa3a
Kiswono Prayogo <kiswono@gmail.com>
2022-12-16 21:46:09 +0700

    Update bundle.go
    fixes https://github.com/datastax/cql-proxy/issues/109

--- astra/bundle.go
+++ astra/bundle.go
@@ -146,6 +146,9 @@ func generateSecureBundleURLWithResponse(url, databaseID, token string, ctx cont
 		return nil, err
 	}
 	res, err := client.GenerateSecureBundleURLWithResponse(ctx, astra.DatabaseIdParam(databaseID))
+	if err != nil {
+		return nil, err
+	}
 
 	if res.StatusCode() != http.StatusOK {
 		return nil, fmt.Errorf("unable to generate bundle urls, failed with status code %d", res.StatusCode())
